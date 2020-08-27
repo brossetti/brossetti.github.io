5332fd31d85661a89010053bca8bee745fc8b40a
Blair Rossetti <rossettib@janelia.hhmi.org>
2020-08-27 18:24:35 -0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+  theme: {
+    extend: {
+      colors: {
+        who: '#191919',
+        what: '#5B5F97',
+        when: '#EFD9CE',
+        where: '#A9FFF7',
+        why: '#43AA8B'
+      }
+    },
+  },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active']
+  },
+  plugins: [],
+}
