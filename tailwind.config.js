004b7081e11420452f233733f5366b06deff9532
Blair Rossetti <rossettib@janelia.hhmi.org>
2020-09-12 16:10:13 -0400

    replaced colors and added custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,22 @@
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
   },
   theme: {
     extend: {
+      fontFamily: {
+        'title': ['Megrim', 'Courier New', 'Courier', 'monospace'],
+        'heading':  ['Do Hyeon', 'Trebuchet MS', 'Helvetica', 'sans-serif'],
+        'body': ['Nanum Gothic', 'Helvetica', 'sans-serif']
+      },
       colors: {
-        space: '#191919',
-        exo: '#5B5F97',
-        thermo: '#D199B6',
-        meso: '#68C3D4',
-        tropo: '#A9FFF7',
-        terra: '#43AA8B'
+        'black': '#191919',
+        'purple': '#5B5F97',
+        'white': '#FFFFFF'
+      },
+      height: {
+        '128': "32 rem",
       }
     },
   },
