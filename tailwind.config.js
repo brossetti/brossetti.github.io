2e875c3dc7b7e9641db706900938d8dd5488f380
Blair Rossetti <rossettib@janelia.hhmi.org>
2020-09-15 13:00:23 -0400

    added layer colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,20 @@ module.exports = {
       colors: {
         'black': '#191919',
         'purple': '#5B5F97',
-        'white': '#FFFFFF'
+        'white': '#FFFFFF',
+        'shadow': 'rgba(0, 0, 0, 0.5)',
+        'spectrum-1': '#D73B48',
+        'spectrum-2': '#E95A54',
+        'spectrum-3': '#FF9760',
+        'spectrum-4': '#F3BB63',
+        'spectrum-5': '#F1CD65',
+        'spectrum-6': '#FFE465',
+        'spectrum-7': '#CFDB5E',
+        'spectrum-8': '#AECD7B',
+        'spectrum-9': '#C0DF9E',
+        'spectrum-10': '#D8F4C2',
+        'spectrum-11': '#D6EACD',
+        'spectrum-12': '#EAF9EA'
       },
       height: {
         '96': "24rem",
@@ -26,7 +39,7 @@ module.exports = {
     },
   },
   variants: {
-    backgroundColor: ['responsive', 'hover', 'focus', 'active']
+    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
   },
   plugins: [],
 }
