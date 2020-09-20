df2c96fb5a3e49970b96a9178d891f7f0c3d664f
Blair Rossetti <rossettib@janelia.hhmi.org>
2020-09-20 19:22:13 -0400

    changed font

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     extend: {
       fontFamily: {
         'title': ['Megrim', 'Courier New', 'Courier', 'monospace'],
-        'heading':  ['Do Hyeon', 'Trebuchet MS', 'Helvetica', 'sans-serif'],
+        'heading':  ['Nanum Gothic Coding', 'Courier New', 'Courier', 'monospace'],
         'body': ['Nanum Gothic', 'Helvetica', 'sans-serif']
       },
       colors: {
@@ -29,7 +29,7 @@ module.exports = {
         'spectrum-12': '#EAF9EA'
       },
       height: {
-        '96': "24rem",
+        '96': '24rem',
       },
       lineHeight: {
         '11': '2.75rem',
