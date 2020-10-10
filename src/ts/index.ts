76946d717369df154118987c8d484f99b17128d1
Blair Rossetti <rossettib@janelia.hhmi.org>
2020-10-10 12:16:14 -0400

    attempting animejs animation

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,5 @@
+import anime from "animejs";
+
 const navbar = document.getElementById("navbar")!;
 const sectionWho = document.getElementById("who")!;
 
@@ -34,3 +36,35 @@ let navObsOptions = {
 const navObs = new IntersectionObserver(navObsCallback, navObsOptions);
 navObs.observe(sectionWho);
 
+// animate where icons
+// const whereIcons = document.querySelector('.where-icon');
+// const icon = () => {
+//   anime({
+//     targets: whereIcons,
+//     strokeDashoffset: [anime.setDashoffset, 0],
+//     easing: 'easeInOutSine',
+//     duration: 1500,
+//     direction: 'alternate',
+//     loop: true
+//   });
+// }
+// if (whereIcons == null) {
+//   console.log('bad')
+// }
+
+var lineDrawing = anime({
+  targets: 'path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutCubic',
+  duration: 4000,
+  begin: function(anim) {
+    document.querySelector('path').setAttribute("stroke", "black");
+    document.querySelector('path').setAttribute("fill", "none");
+  },
+  complete: function(anim) {
+    document.querySelector('path').setAttribute("fill", "yellow");
+  },
+  autoplay: false
+});
+
+document.querySelector('.where-icon').onload = lineDrawing.restart;
